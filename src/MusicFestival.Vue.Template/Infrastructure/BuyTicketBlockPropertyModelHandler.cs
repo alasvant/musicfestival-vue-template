744be102b543f361158708229eb83fbe59e1ec8b
Antti Alasvuo <alasvuo@gmail.com>
2018-12-06 13:02:58 +0200

    Old changes to original master which were not properly done to a feature branch.

---
+++ src/MusicFestival.Vue.Template/Infrastructure/BuyTicketBlockPropertyModelHandler.cs
@@ -0,0 +1,56 @@
+﻿using EPiServer.ContentApi.Core.Serialization;
+using EPiServer.ContentApi.Core.Serialization.Models;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.SpecializedProperties;
+using MusicFestival.Template.Models.Blocks;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MusicFestival.Template.Infrastructure
+{
+    /// <summary>
+    /// Used to support conversion of <see cref="MusicFestival.Template.Infrastructure.BuyTicketBlockPropertyModel"/>
+    /// which is returned from API as a property of LandingPage
+    /// </summary>
+    public class BuyTicketBlockPropertyModelHandler: IPropertyModelConverter
+    {
+        /// <summary>
+        /// Verifies that the instance of IPropertyModelHandler has the correct IPropertyModel registered for the provided PropertyData type.
+        /// </summary>
+        /// <param name="propertyData">An instance of PropertyData to check</param>
+        /// <returns></returns>
+        public bool HasPropertyModelAssociatedWith(PropertyData propertyData)
+        {
+            var blockPropertyData = propertyData as PropertyBlock;
+
+            if (!(blockPropertyData?.Value is BuyTicketBlock))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Converts an instance of PropertyData into a specific Property Model
+        /// </summary>
+        /// <param name="propertyData">An instance of PropertyData</param>
+        public IPropertyModel ConvertToPropertyModel(PropertyData propertyData, CultureInfo language, bool excludePersonalizedContent, bool expand)
+        {
+            return new BuyTicketBlockPropertyModel((PropertyBlock) propertyData);
+        }
+
+        public int SortOrder => 1000;
+
+        public IEnumerable<TypeModel> ModelTypes => new List<TypeModel>
+        {
+            new TypeModel
+            {
+                ModelType = typeof(BlockPropertyDefinitionType),
+                ModelTypeString = nameof(BlockPropertyDefinitionType),
+                PropertyType = typeof(BuyTicketBlock)
+            }
+        };
+    }
+}
---
+++ src/MusicFestival.Vue.Template/Infrastructure/SiteInitialization.cs
@@ -0,0 +1,87 @@
+﻿using EPiServer.ContentApi.Core.Configuration;
+using EPiServer.ContentApi.Core.Serialization;
+using EPiServer.ContentApi.Search;
+using EPiServer.Core;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+using MusicFestival.Template.Infrastructure.WebApi;
+using MusicFestival.Template.Models;
+using Newtonsoft.Json;
+using System;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MusicFestival.Template.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(ServiceContainerInitialization))]
+    public class SiteInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.Configure<ContentApiConfiguration>(config =>
+            {
+                config.Default()
+                    .SetMinimumRoles(string.Empty)
+                    .SetRequiredRole("Content Api Access")
+                    .SetMultiSiteFilteringEnabled(true)
+                    .SetSiteDefinitionApiEnabled(true);
+            });
+
+            // Register the extended content model mapper to be able to provide custom models from content api
+            context.Services.Intercept<IContentModelMapper>((locator, defaultModelMapper) =>
+                new ExtendedContentModelMapper(
+                    locator.GetInstance<IUrlResolver>(),
+                    defaultModelMapper,
+                    locator.GetInstance<ServiceAccessor<HttpContextBase>>(),
+                    locator.GetInstance<IContentVersionRepository>()
+                    )
+            );
+
+            context.Services.Configure<ContentApiSearchConfiguration>(config =>
+            {
+                config.Default().SetSearchCacheDuration(TimeSpan.Zero);
+            });
+
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.StructureMap()));
+
+            GlobalConfiguration.Configure(config =>
+            {
+                config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.LocalOnly;
+                config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings();
+                config.Formatters.XmlFormatter.UseXmlSerializer = true;
+                config.DependencyResolver = new StructureMapResolver(context.StructureMap());
+                config.MapHttpAttributeRoutes();
+                config.EnableCors();
+            });
+            context.Services.AddTransient<IPropertyModelConverter, BuyTicketBlockPropertyModelHandler>();
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            var options = ServiceLocator.Current.GetInstance<DisplayOptions>();
+            options
+                .Add("full", "Full", ContentAreaTags.FullWidth, "", "epi-icon__layout--full")
+                .Add("wide", "Wide", ContentAreaTags.TwoThirdsWidth, "", "epi-icon__layout--two-thirds")
+                .Add("half", "Half", ContentAreaTags.HalfWidth, "", "epi-icon__layout--half")
+                .Add("narrow", "Narrow", ContentAreaTags.OneThirdWidth, "", "epi-icon__layout--one-third");
+
+            AreaRegistration.RegisterAllAreas();
+        }
+
+        public void Uninitialize(InitializationEngine context) { }
+
+        public static class ContentAreaTags
+        {
+            public const string FullWidth = "u-md-sizeFull";
+            public const string TwoThirdsWidth = "u-md-size2of3";
+            public const string HalfWidth = "u-md-size1of2";
+            public const string OneThirdWidth = "u-md-size1of3";
+            public const string NoRenderer = "norenderer";
+        }
+    }
+}
